4adc249543191606cb14d0bf7ffc709ebbf9f29b
Kibet 1998 <ronoronald1998@gmail.com>
2021-07-13 10:49:45 +0300

    add profile.html

--- connect/urls.py
+++ connect/urls.py
@@ -1,1 +1,3 @@
 url(r'^register/$',views.register,name='register'),
+url(r'^login',views.loginpage,name='loginpage'),
+url(r'^profile/&',views.profilepage,name='profilepage'),
--- connect/views.py
+++ connect/views.py
@@ -19,3 +19,41 @@ def register(request):
     context={'form':form}
     
     return render(request,'register.html')
+
+def loginpage(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=  request.POST.get('password')
+
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect(reverse('welcome'))
+        else:
+            messages.info(request,'Username or password is incorrect')
+       
+    context={}
+    return render(request,'login.html',context)
+
+def profilepage(request,username):
+    user=User.objects.filter(username=username)
+    if user:
+    # current_user = request.user
+        profile = Profile.objects.get(user=user)
+        bio=profile.bio
+        
+    if request.method == "POST":
+        form=ImageForm(data=request.POST,files=request.FILES)
+        if form.is_valid():
+            form.save()
+            obj=form.instance
+            return redirect(reverse("profile.html",{"obj":obj}))
+        else:
+            form=ImageForm()
+        img=Image.objects.all()
+        return render(request,'profile.html', {'img':img,'form':form})
+    
+
+    context={'u_form':u_form,'p_form':p_form,'current_user':current_user,'profile':profile}
+
+    return render(request,'profile.html',context=context)
