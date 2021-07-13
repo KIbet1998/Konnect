fcc58cbb7812ebe866ebb6f05cee28314b9a1c21
Kibet 1998 <ronoronald1998@gmail.com>
2021-07-13 10:54:08 +0300

    add search.html

--- connect/urls.py
+++ connect/urls.py
@@ -1,3 +1,4 @@
 url(r'^register/$',views.register,name='register'),
 url(r'^login',views.loginpage,name='loginpage'),
 url(r'^profile/&',views.profilepage,name='profilepage'),
+url(r'^search/', views.search_results, name='search_results'),
